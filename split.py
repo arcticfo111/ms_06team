0b0ffbea2d23c84a1afde2f6798046a14748ee1f
arcticfo111 <arcticfo111@gmail.com>
2023-08-08 20:18:24 +0900

    <feat>:<mac setting, multi_car train & test>
    - mac에서 train.py와 test.py를 실행할 수 있게 데이터 경로 설정
    - 주변 차량까지 라벨링(번호판과 차를 클래스로)한 데이터 multi_car(재호님 공유)사용하여 학습
        및 테스트

--- split.py
+++ split.py
@@ -1,10 +1,11 @@
 import os # os 관련 기능 사용
 import random
-import shutil
-from glob import glob
+import shutil # 파일을 복사하거나 이동할 수 있는 모듈
+from glob import glob # 경로명 패턴으로 파일을 찾을 수 있는 함수
 
-
+# yolo 데이터셋을 분할하는 함수 정의
 def split_yolo_dataset(path, images_path, labels_path, train_pct=0.8, val_pct=0.1):
+    ## 필요한 디렉토리 생성, exist_ok=True으로 이미 디렉토리가 존재할시 오류가 발생하지 않도록 하기
     os.makedirs(os.path.join(path,"train/images"), exist_ok=True)
     os.makedirs(os.path.join(path,"train/labels"), exist_ok=True)
     
@@ -14,17 +15,22 @@ def split_yolo_dataset(path, images_path, labels_path, train_pct=0.8, val_pct=0.
     os.makedirs(os.path.join(path,"val/images"), exist_ok=True)
     os.makedirs(os.path.join(path,"val/labels"), exist_ok=True)
 
+    ## 지정된 경로에서 png 파일 가져오기
     images = glob(images_path + "/*.png")
+    ## 이미지 무작위로 섞기
     random.shuffle(images)
+    ## 전체 이미지 데이터를 train, val, test로 나누기 위해 필요한 이미지 수 계산하기
     num_images = len(images)
     num_train = int(num_images * train_pct)
     num_val = int(num_images * val_pct)
     num_test = num_images - (num_train + num_val)
 
+    ## 섞인 이미지 목록을 train, val, test 세트로 분할하기
     train_imgs = images[:num_train]
     val_imgs = images[num_train:num_train + num_val]
     test_imgs = images[num_train + num_val:]
 
+    ## 각 세트에 대해 이미지와 레이블 파일을 train, val, test 디렉토리로 복사하기
     for train_img in train_imgs:
         basename = os.path.basename(train_img).split(".")[0]
         shutil.copy(train_img, os.path.join(path,"train/images"))
@@ -40,9 +46,8 @@ def split_yolo_dataset(path, images_path, labels_path, train_pct=0.8, val_pct=0.
         shutil.copy(test_img, os.path.join(path,"test/images"))
         shutil.copy(os.path.join(labels_path, basename + ".txt"), os.path.join(path,"test/labels"))
 
-
 if __name__ == "__main__":
-    path = "./ultralytics/cfg/car_plate_dataset"
+    path = "/Users/dobby/Library/Mobile Documents/com~apple~CloudDocs/electronicsandcode/code/arcticfo111/ms_06team/ms_team_cardata/final_data_combination1/multi_car"
     images_path = os.path.join(path,"images")
     labels_path = os.path.join(path,"labels")
 
